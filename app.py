34bebb3831e89dec84b881b902b7b75b4f3978ff
raven4241 <106502737+raven4241@users.noreply.github.com>
2022-09-19 20:50:38 +0530

    update app.py

--- app.py
+++ app.py
@@ -3,5 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.route('/')
+def landing_page():
+  return render_template('home.html')
+
 if __name__ == "__main__":
   app.run(debug=True, host='0.0.0.0')
